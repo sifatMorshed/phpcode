63be332059c1ad9ba58ccb9378263d68535a2151
sifatMorshed <sifatMorshed@users.noreply.github.com>
2020-11-21 18:36:03 +0600

    if else

--- new.php
+++ new.php
@@ -1,8 +1,38 @@
 <?php
-$d = 5;
-$r=6;
 
-echo d
-echo "dd"
+//phpinfo();
+
+$name = 'cats';
+$age=2;
+
+if (strtolower($name)=='cat') 
+{
+    if ($age>=21)
+    {
+      echo 'you\'r over 21';
+    
+if(1==1)
+
+{
+ echo '1 is 1';
+}
+
+else 
+{
+    echo 'not voer 21';
+}
+
+}
+
+else 
+{
+    echo 'you\r not cat';
+}
+
+}
 
 ?>
+
+<html>
+
+</html>
