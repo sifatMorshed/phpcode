96166895a222e806a4d47408da36329e09df23b6
sifatMorshed <sifatMorshed@users.noreply.github.com>
2020-11-22 02:59:29 +0600

    exp

---
+++ exp.php
@@ -0,0 +1,14 @@
+<?php
+
+// echo "hello";
+
+// die();
+
+// echo "world";
+
+mysqli_connect('localhost', 'roott', '') or exit ('die!!');
+
+echo "connected";
+
+?>
+
---
+++ swtich28.php
@@ -0,0 +1,28 @@
+<?php
+
+$c='wed';
+
+switch ($c) {
+
+case 'sun':
+
+    echo "weekend";
+
+break;
+
+case 'mon':
+
+    echo "weekend";
+
+break;
+
+default:
+
+echo "it's workday";
+
+break;
+
+}
+
+?>
+
