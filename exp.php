31f31c9d1a2898218ebcb0ab1279db1ce3480852
sifatMorshed <sifatMorshed@users.noreply.github.com>
2020-11-23 03:31:04 +0600

    diff

--- exp.php
+++ exp.php
@@ -1,14 +1,24 @@
 <?php
 
+$s = "this ssss apple";
+
 // echo "hello";
 
 // die();
 
 // echo "world";
 
-mysqli_connect('localhost', 'roott', '') or exit ('die!!');
+// mysqli_connect('localhost', 'roott', '') or exit ('die!!');
 
-echo "connected";
+// echo "connected";
+
+if (preg_match('/apple/', $s)) {
+    echo "match!";
+}
+
+else {
+    echo "not match";
+}
 
 ?>
 
